73442e690c22e12c781628162f5ce273a89fe2ec
Elizaveta-Dem <e.lizaveta.demyanova.03@gmail.com>
2023-09-08 19:13:27 +0300

    practic_08.09

---
+++ lecture_4/nes.py
@@ -0,0 +1,20 @@
+# alphavit = {
+#     1: 'A',
+#     2: 'B',
+#     3: 'C',
+#     4: 'X',
+#     10: 'Y',
+# }
+
+# print(alphavit)
+
+alphavit = {
+    1: 'A',
+    2: 'B',
+    3: 'C',
+    4: 'X',
+    10: 'Y',
+}
+
+for pair in alphavit.items():
+    print(pair)
