bb3fa394afd12abc19db68ae7935db7deb1c8cea
Elizaveta-Dem <e.lizaveta.demyanova.03@gmail.com>
2023-09-10 14:47:22 +0300

    dz_10.09

--- dz/time.py
+++ dz/time.py
@@ -1,8 +1,17 @@
+# def time_conversion(sec):
+#     hour_time = sec // 3600
+#     min_time = (sec % 3600) // 60
+#     return str(hour_time) + 'ч. ' + str(min_time) + "мин. "
+
+# list_time = [3600, 3601, 3500, 323500]
+# for i in list_time:
+#     print(i,'=', time_conversion(i))
+
 def time_conversion(sec):
     hour_time = sec // 3600
     min_time = (sec % 3600) // 60
-    return str(hour_time) + 'ч. ' + str(min_time) + "мин. "
+    return str(hour_time) + ' час(а/ов) ' + str(min_time) + " минут(а/ы) "
 
 list_time = [3600, 3601, 3500, 323500]
 for i in list_time:
-    print(i,'=', time_conversion(i))
+    print(i,'-->', time_conversion(i))
