9dc6e10263d8e1523f39d57e3576937a2aa3aab9
Elizaveta-Dem <e.lizaveta.demyanova.03@gmail.com>
2023-12-21 15:55:23 +0300

    чат бот

--- dz/kvadrat_elementov.py
+++
@@ -1,6 +0,0 @@
-def squares(N):
-    l: list = []
-    for i in range(-N, N+1): #положительные и отрицательные числа на вход
-        l.append(i**2)
-    return l
-print(squares(2))
--- lecture_2/06.09.py
+++
@@ -1,9 +0,0 @@
-# #_set1: set =set([i, f, s, b, n])
-
-# _matrix = [
-#     'VIP matrix'
-#     [1,2,3],
-#     [2,3,4],
-#     ['s',4,5],
-#     'saddas'
-# ]
